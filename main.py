9c23e06b052969f28b7d95123c8e396b7c3bf8d1
Dan <djlafferty7@gmail.com>
2022-01-08 01:24:51 +0000

    updated debug and completed basic template

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    message = 'A message from the backend'
+    return render_template('index.html', message=message)
+
 
 if __name__ == '__main__':
-    app.run(host'127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
